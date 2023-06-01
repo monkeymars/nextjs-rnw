734d479e44781ce52a54452639ac47c5a1e78ff3
Gery Wibowo <gery.wibowo@lemonilo.com>
2023-06-01 22:12:43 +0700

    fix: module config alias

--- next.config.js
+++ next.config.js
@@ -1,18 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  transpilePackages: ["rn-grwb-feeds", "nativewind"],
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
-    }
+      "react-native$": "react-native-web",
+      "rn-grwb-feeds": "rn-grwb-feeds",
+    };
     config.resolve.extensions = [
-      '.web.js',
-      '.web.jsx',
-      '.web.ts',
-      '.web.tsx',
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
       ...config.resolve.extensions,
-    ]
-    return config
+    ];
+    return config;
   },
-}
+};
